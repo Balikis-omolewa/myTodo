32aa5ecd7e267e611d1540e46e374fdde705a08a
Hi, I'm Balikis Omolewa <106351278+Balikis-omolewa@users.noreply.github.com>
2024-08-17 05:13:06 +0100

    Update TaskForm.jsx
    to-do app initial commite

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,31 +1,37 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-function TaskForm({ addTask }) {
-  const [taskName, setTaskName] = useState('');
-  const [taskDescription, setTaskDescription] = useState('');
-  const [taskDate, setTaskDate] = useState(new Date().toISOString().split('T')[0]);
+function TaskForm({ addTask, existingTask }) {
+  // State initialization
+  const [taskName, setTaskName] = useState(existingTask ? existingTask.name : '');
+  const [taskDescription, setTaskDescription] = useState(existingTask ? existingTask.description : '');
+  const [dueDate, setDueDate] = useState(existingTask ? existingTask.dueDate : '');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     // Validate the task name and description before adding it to the list
-    if (taskName && taskDescription) {
+    if (taskName && taskDescription && dueDate) {
       const newTask = {
-        id: Date.now(),
+        id: Date.now(), // Generate a unique ID
         name: taskName,
         description: taskDescription,
+        dueDate: dueDate, // Include due date
         completed: false,
       };
+
       // Add the new task to the list using the addTask prop function
       addTask(newTask);
+
+      // Reset the form fields
       setTaskName('');
       setTaskDescription('');
+      setDueDate('');
     } else {
       alert('Please fill all fields');
     }
   };
 
   return (
-    // Task form with input fields for task name, description, and due date
     <form onSubmit={handleSubmit} className="task-form">
       <input
         type="text"
@@ -37,16 +43,14 @@ function TaskForm({ addTask }) {
         type="text"
         placeholder="Task Description"
         value={taskDescription}
-        className='description'
+        className="description"
         onChange={(e) => setTaskDescription(e.target.value)}
       />
-      <span style={{textAlign: "left", fontSize: "15px", 
-         fontWeight: "400", color: "#d2cee9"}}>Add due date</span>
-       <input
+      <span style={{ textAlign: "left", fontSize: "15px", fontWeight: "400", color: "#d2cee9" }}>Add due date</span>
+      <input
         type="date"
-        placeholder='Add due date'
-        value={taskDate}
-        onChange={(e) => setTaskDate(e.target.value)}
+        value={dueDate}
+        onChange={(e) => setDueDate(e.target.value)}
       />
       <button type="submit">Add Task</button>
     </form>
